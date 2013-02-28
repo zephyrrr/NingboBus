ffbfee332b6bc66c92651b111b022acc7e0cbea1
Zephyrrr <zephyrrr@163.com>
2013-02-28 10:54:18 +0800

    Reference

--- NbBusConsole/Program.cs
+++ NbBusConsole/Program.cs
@@ -18,6 +18,8 @@ namespace NbBusConsole
 
             var x = NbBusService.NingboBusHelper.Instance.GetBusLocations(35); // 15路(锦江年华始发站=>汽车东站始发站)
             var s = NbBusService.NingboBusHelper.Instance.ConvertBusLocationsToString(x);
+            Console.WriteLine(s);
+            Console.ReadLine();
         }
     }
 }
